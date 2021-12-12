f358f1af6ecc0c78b9c73d351d2f7b81402b2dc4
Matko Piskor <matko.piskor@gmail.com>
2021-12-12 21:08:28 +0100

    Lang selector

--- src/components/language-selector/LanguageSelector.tsx
+++ src/components/language-selector/LanguageSelector.tsx
@@ -3,8 +3,28 @@ import { FC } from 'react';
 import { useLanguageSelector } from './useLanguageSelector';
 
 import './LanguageSelector.css';
+import { TopbarButton } from '../topbar-button';
 
 export const LanguageSelector: FC = () => {
-    const { active, list } = useLanguageSelector();
-    return <div></div>;
+    const { list, active, setActive, activeLangName, open, toggleOpen, currRef } = useLanguageSelector();
+    return (
+        <div ref={currRef} className="languages-selector">
+            <TopbarButton open={open} label={activeLangName} onClick={() => toggleOpen()} />
+            {open && (
+                <div className="languages-selector__list">
+                    {list.map(({ id, name }) => (
+                        <div
+                            key={id}
+                            className={`languages-selector__item${
+                                active === id ? ' languages-selector__item-active' : ''
+                            }`}
+                            onClick={() => setActive(id)}
+                        >
+                            {name}
+                        </div>
+                    ))}
+                </div>
+            )}
+        </div>
+    );
 };
--- src/components/language-selector/useLanguageSelector.ts
+++ src/components/language-selector/useLanguageSelector.ts
@@ -1,9 +1,48 @@
-import { useAppSelector } from '../../redux/hooks';
+import { useCallback, useMemo, useRef, useState } from 'react';
+import { useAppDispatch, useAppSelector } from '../../redux/hooks';
+import { setActive as setActiveLang } from '../../redux/reducers/langugagesReducer';
+import { useOutsideClickListener } from '../../utils/useOutsideClickListener';
 
 export const useLanguageSelector = () => {
+    const dispatch = useAppDispatch();
     const { list, active } = useAppSelector((state) => state.languages);
+    const [open, setOpen] = useState(false);
+    const currRef = useRef(null);
+
+    const toggleOpen = useCallback(
+        (forceClose?: boolean) => {
+            console.log(forceClose);
+            if (forceClose) {
+                setOpen(false);
+                return;
+            }
+            setOpen(!open);
+        },
+        [open]
+    );
+
+    const activeLangName = useMemo(() => {
+        const { name } = list.find(({ id }) => id === active)!;
+        return name;
+    }, [list, active]);
+
+    const setActive = useCallback(
+        (id: number) => {
+            dispatch(setActiveLang(id));
+            toggleOpen(true);
+        },
+        [dispatch, toggleOpen]
+    );
+
+    useOutsideClickListener(currRef, () => toggleOpen(true));
+
     return {
         list,
+        activeLangName,
         active,
+        setActive,
+        open,
+        toggleOpen,
+        currRef,
     };
 };

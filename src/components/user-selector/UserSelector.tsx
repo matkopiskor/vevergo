9b1307b311d7255a0f48d16dc3c724da44fab369
Matko Piskor <matko.piskor@gmail.com>
2021-12-13 20:34:14 +0100

    Update

--- src/components/user-selector/UserSelector.tsx
+++ src/components/user-selector/UserSelector.tsx
@@ -12,7 +12,7 @@ export const UserSelector: FC = () => {
             {open && (
                 <div className="user-selector__list">
                     <div key="1" className="user-selector__item">
-                        Last update: 12.12.2021. 9:24
+                        Last update: 13.12.2021. 20:34
                     </div>
                 </div>
             )}

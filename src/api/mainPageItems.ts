203167aa539b3864c34d40de8614de8e32f9a5d2
Matko Piskor <matko.piskor@gmail.com>
2021-12-14 20:46:05 +0100

    Fix sort

--- src/api/mainPageItems.ts
+++ src/api/mainPageItems.ts
@@ -23,18 +23,18 @@ const getMainPageItems = async (params?: Params): Promise<AxiosResponse<IMainPag
 
     if (sortBy) {
         const { orderType, orderItem } = sortByOptions.find(({ value }) => value === sortBy)!;
-        inputParams['orderType'] = orderType;
-        inputParams['orderItem'] = orderItem;
+        inputParams['order_type'] = orderType;
+        inputParams['order_item'] = orderItem;
     }
 
     if (searchText) {
         inputParams['search_text'] = searchText;
     }
 
-    inputParams['currency'] = currency;
-
     const url = Object.keys(inputParams).length === 0 ? 'view/recent_item_listings' : 'view/search_item_listings';
     const method = Object.keys(inputParams).length === 0 ? 'GET' : 'POST';
+
+    inputParams['currency'] = currency;
 
     return ApiService<IMainPageData>(method, url, inputParams, null, headers);
 };

706f7da152b3c08612bfd30cbb0ae7d03f5bb952
Matko Piskor <matko.piskor@gmail.hr>
2021-12-27 11:11:04 +0100

    Fix breadcrumbs css

--- src/components/breadcrumbs/Breadcrumbs.tsx
+++ src/components/breadcrumbs/Breadcrumbs.tsx
@@ -6,12 +6,15 @@ import { useBreadcrumbs } from './useBreadcrumbs';
 
 export const Breadcrumbs: FC = () => {
     const { breadcrumbs, onBreadcrumbClick } = useBreadcrumbs();
+    if (breadcrumbs.length === 0) {
+        return null;
+    }
     return (
-        <div className="breadcrumbs">
+        <div className='breadcrumbs'>
             {breadcrumbs.map(({ id, name }, idx, src) => {
                 return (
                     <div key={id + '_' + idx} style={{ display: 'flex', alignItems: 'center' }}>
-                        <div className="breadcrumbs__item" onClick={() => onBreadcrumbClick(id)}>
+                        <div className='breadcrumbs__item' onClick={() => onBreadcrumbClick(id)}>
                             {name}
                         </div>
                         {idx !== src.length - 1 && <ChevronsRight size={14} />}

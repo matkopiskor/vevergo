b85600e5e8a91859f062f5801cebe5211b6434b0
Matko Piskor <matko.piskor@gmail.com>
2021-12-31 12:51:32 +0100

    update

--- src/components/user-selector/UserSelector.tsx
+++ src/components/user-selector/UserSelector.tsx
@@ -5,14 +5,14 @@ import { useUserSelector } from './useUserSelector';
 export const UserSelector: FC = () => {
     const { currRef, imgSrc, open, toggleOpen } = useUserSelector();
     return (
-        <div ref={currRef} className='user-selector'>
-            <div className='user-selector__avatar' onClick={() => toggleOpen()}>
-                <img src={imgSrc} alt='avatar' />
+        <div ref={currRef} className="user-selector">
+            <div className="user-selector__avatar" onClick={() => toggleOpen()}>
+                <img src={imgSrc} alt="avatar" />
             </div>
             {open && (
-                <div className='user-selector__list'>
-                    <div key='1' className='user-selector__item'>
-                        Last update: 27.12.2021. 15:14 Common filters working (style not done)
+                <div className="user-selector__list">
+                    <div key="1" className="user-selector__item">
+                        Last update: 30.12.2021. 12:50 Other filters UI (missing implementation), fixes
                     </div>
                 </div>
             )}

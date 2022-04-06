228291c6ce736341f61bd828bb69caf97f20a2cb
Matko Piskor <matko.piskor@gmail.com>
2022-04-06 20:39:38 +0200

    Add change email screen

---
+++ src/api/changeEmailConfirmation.ts
@@ -0,0 +1,12 @@
+import { ApiService } from '.';
+import { ERROR_CODES } from '../constants/errorCodes';
+import { notify } from '../services/notifications';
+
+export const changeEmailConfirmation = async (data: { email: string; confirmation_id: string }) => {
+    const resp = await ApiService<any>({ method: 'PUT', url: 'user/registration_confirmation', data });
+    if ((resp as any)?.data?.error_id && (resp as any)?.data?.error_id !== 0) {
+        notify({ type: 'WARNING', description: ERROR_CODES[(resp as any)?.data?.error_id] });
+        return false;
+    }
+    return true;
+};
